dbd09c7ddf309bb691e6aca4361098b9208b5e80
Dieg0MV <diegoma.v2004@gmail.com>
2023-08-07 15:57:18 -0600

    esta es la base de el porfolio aun le hace falta disain

---
+++ js/script.js
@@ -0,0 +1,9 @@
+io =document.getElementById("iam");
+bio =document.getElementById("how")
+io.addEventListener("click", hola);
+
+function hola(){
+    
+    bio.innerHTML="Esto sera un secreto para los mas curiosos"; 
+
+}
