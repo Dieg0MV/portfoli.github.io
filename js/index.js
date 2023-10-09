632046b41be790f4bddc09d6a1af1298b81c025a
Diego <101619179+Dieg0MV@users.noreply.github.com>
2023-10-08 19:39:45 -0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+
+window.sr = ScrollReveal();
+  sr.reveal('.content', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
+  
+    sr.reveal('.contac', {
+    duration: 3000,
+    origin: 'bottom',
+    distance: '-100px'
+  });
