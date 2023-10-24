121acbfa414294aa19bd96f7bb197550336ef653
Diego <101619179+Dieg0MV@users.noreply.github.com>
2023-10-23 22:51:57 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,6 +4,6 @@ io.addEventListener("click", hola);
 
 function hola(){
     
-    bio.innerHTML=" spy un gran candidato por que puedo aprender php en una noche, wordpress es re easy y por que me gusta solucionar problemas de codigo y crear codigo limpio y escalable "; 
+    bio.innerHTML=" soy un gran candidato por que puedo aprender php en una noche, wordpress es re easy y por que me gusta solucionar problemas de codigo y crear codigo limpio y escalable "; 
 
 }
